e6e017e3cb55547ae8b79a881b8db9cc67034b56
Hitesh <hitheshtr04@gmail.com>
2024-07-02 00:21:02 +0530

    React returns

---
+++ 6.1/hooks_lean_app/src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  return (
+    <>
+    <Header title="hello"></Header>
+    <Header title="Hi"></Header>
+    </>
+  )
+}
+
+function Header({title}){
+  return <div>
+    {title}
+  </div>
+}
+
+export default App
