3fd12bc22395dcaec64f4d037b533ced5f492381
Hitesh <hitheshtr04@gmail.com>
2024-07-02 23:43:38 +0530

    memo

--- 6.1/hooks_lean_app/src/App.jsx
+++ 6.1/hooks_lean_app/src/App.jsx
@@ -2,35 +2,29 @@ import React, { Fragment, useState } from "react"
 
 function App() {
 
-  return (
-    <Fragment>
-      <HeaderWithButton></HeaderWithButton>
-      <Header title="Extra title 1" ></Header>
-      <Header title="Extra title 2" ></Header>
-      <Header title="Extra title 3" ></Header>
-    </Fragment>
-  )
-}
-
-function HeaderWithButton(){
   const [title, setTitle] = useState("My name is Hitesh")
 
   function updateTitle(){
     setTitle("My name is "+ Math.random())
   }
 
-  return <div>
-    <Header title="Hi"></Header>
-    <button onClick={updateTitle}>Update the Title</button>
-     <Header title={title}></Header>
-  </div>
-  
+  return (
+    <>
+      <button onClick={updateTitle}>Update the Title</button>
+      <Header title={title}></Header>
+
+      <Header title="Hi"></Header>
+      <Header title="Hi"></Header>
+      <Header title="Hi"></Header>
+      <Header title="Hi"></Header>
+    </>
+  )
 }
 
-function Header({title}){
+const Header = React.memo(function Header({title}){
   return <div>
     {title}
   </div>
-}
+})
 
 export default App
