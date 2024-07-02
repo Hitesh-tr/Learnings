f907074bb168a17dbc39eeee2c40be4516748738
Hitesh <hitheshtr04@gmail.com>
2024-07-02 23:35:12 +0530

    memimize re rerender

--- 6.1/hooks_lean_app/src/App.jsx
+++ 6.1/hooks_lean_app/src/App.jsx
@@ -1,12 +1,30 @@
+import React, { Fragment, useState } from "react"
 
 function App() {
 
   return (
-    <>
-    <Header title="hello"></Header>
-    <Header title="Hi"></Header>
-    </>
+    <Fragment>
+      <HeaderWithButton></HeaderWithButton>
+      <Header title="Extra title 1" ></Header>
+      <Header title="Extra title 2" ></Header>
+      <Header title="Extra title 3" ></Header>
+    </Fragment>
   )
+}
+
+function HeaderWithButton(){
+  const [title, setTitle] = useState("My name is Hitesh")
+
+  function updateTitle(){
+    setTitle("My name is "+ Math.random())
+  }
+
+  return <div>
+    <Header title="Hi"></Header>
+    <button onClick={updateTitle}>Update the Title</button>
+     <Header title={title}></Header>
+  </div>
+  
 }
 
 function Header({title}){
