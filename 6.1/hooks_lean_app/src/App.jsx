f461eec0d5df6ed3ce3de6dbdb9bd5132684ce7b
Hitesh <hitheshtr04@gmail.com>
2024-07-24 23:12:41 +0530

    Key Changes - todo app

--- 6.1/hooks_lean_app/src/App.jsx
+++ 6.1/hooks_lean_app/src/App.jsx
@@ -2,29 +2,47 @@ import React, { Fragment, useState } from "react"
 
 function App() {
 
-  const [title, setTitle] = useState("My name is Hitesh")
+  const [todos, setTodos] = useState([{
+    id: 1,
+    title: "Wake up at 6AM",
+    desc:"wakeup at 6AM and go to gym"
+  },
+  {
+    id: 2,
+    title: "Study",
+    desc:"wakeup at 6AM and go to gym and study"
+  },
+  {
+    id: 3,
+    title: "Code",
+    desc:"wakeup at 6AM and go to gym and study and the COde React"
+  }])
 
-  function updateTitle(){
-    setTitle("My name is "+ Math.random())
+  function addTodo() {
+    setTodos([...todos, {
+      id: todos.length + 1,
+      title: Math.random(),
+      desc: Math.random()
+    }])
   }
 
   return (
     <div>
-      <button onClick={updateTitle}>Update the Title</button>
-      <Header title={title}></Header>
-
-      <Header title="Hi"></Header>
-      <Header title="Hi"></Header>
-      <Header title="Hi"></Header>
-      <Header title="Hi"></Header>
+      <button onClick={addTodo}>Add Todo</button>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} desc={todo.desc} />
+      ))}
     </div>
   )
 }
 
-const Header = React.memo(function Header({title}){
-  return <div>
-    {title}
-  </div>
-})
+//Key to identify each element in the array - removes error key={todo.id} => id: 3,
 
+function Todo({title, desc}){
+  return <div>
+    <input type="checkbox"></input>
+    <h5>{title}</h5>
+    <p>{desc}</p>
+  </div>
+}
 export default App
